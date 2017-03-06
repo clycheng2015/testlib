6f2054695cde98321a00540869ec3a1e076c9584
liangyancheng <liangyan.cheng@uebuycar.com>
2017-03-06 16:38:28 +0800

    测试修改并远程提交

--- app/src/main/assets/read_me.txt
+++ app/src/main/assets/read_me.txt
@@ -5,3 +5,19 @@
 2、进入Repositories，点击右侧“new”，输入一个仓库名称，创建当前仓库。
 
 3、创建后仓库后，就有对应的仓库地址，在VCS中checkout from version contorl检出代码
+
+如何创建自己的Github代码仓库并可以被其他人作为库引用
+
+1、新建一个工程获取new Module
+
+2、在VCS---inport into version contorl---share project on Gitbub，填上仓库名称，点击share
+
+3、选择要加入到库中 的文件
+
+4、登录Github，找到 release,创建一个release,选择publish release
+
+5、接下来需要登录到JitPack.io网站，输入项目地址，点击get it。
+
+6、如果是Maven引用，则在根目录下配置：https://jitpack.io,如果是Gradle,则compile '仓库的完整地址：'
+
+示例参考：https://jitpack.io/#clycheng2015/GithubUpload/v1.0，这里有详细的说明。
---
+++ testlib/src/main/java/com/cly/testlib/QRcodeUtil.java
@@ -0,0 +1,50 @@
+package com.cly.testlib;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+/**
+ * 项目名称：app_android_dealer
+ * 类描述：根据字符串信息生成二维码图片
+ * 创建人：Administrator
+ * 创建时间：2017-02-27
+ */
+
+
+public class QRcodeUtil {
+    private static String TAG = QRcodeUtil.class.getSimpleName();
+    private static Context mContext;
+    private static final int QR_WIDTH = 300;
+    private static final int QR_HEIGHT = 300;
+
+    public static Bitmap generateQRCode(Context context, String content) {
+        Bitmap bitmap = null;
+        mContext = context;
+//        int qrWidth = DisplayUtils.dip2px(mContext, QR_WIDTH);
+//        int qrHeight = DisplayUtils.dip2px(mContext, QR_HEIGHT);
+//        try {
+//            Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
+//            hints.put(EncodeHintType.CHARACTER_SET, HmcConstants.CHARSET_UTF8);
+//            // 需要引入core包
+//            // 把输入的文本转为二维码
+//            BitMatrix bitMatrix = new QRCodeWriter().encode(content,
+//                    BarcodeFormat.QR_CODE, qrWidth, qrHeight, hints);
+//            LogUtil.i(TAG, "QRw:" + bitMatrix.getWidth() + "QRh:" + bitMatrix.getHeight());
+//            int[] pixels = new int[qrWidth * qrHeight];
+//            for (int y = 0; y < qrHeight; y++) {
+//                for (int x = 0; x < qrWidth; x++) {
+//                    if (bitMatrix.get(x, y)) {
+//                        pixels[y * qrWidth + x] = 0xff000000;
+//                    } else {
+//                        pixels[y * qrWidth + x] = 0xffffffff;
+//                    }
+//                }
+//            }
+//            bitmap = Bitmap.createBitmap(qrWidth, qrHeight, Bitmap.Config.ARGB_8888);
+//            bitmap.setPixels(pixels, 0, qrWidth, 0, 0, qrWidth, qrHeight);
+//        } catch (WriterException e) {
+//            e.printStackTrace();
+//        }
+        return bitmap;
+    }
+}

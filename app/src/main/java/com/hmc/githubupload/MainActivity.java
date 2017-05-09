6d8e694fac50ac5e649d4827f2e14186211a4b6e
liangyancheng <liangyan.cheng@uebuycar.com>
2017-05-09 13:23:16 +0800

    测试提交1

--- app/src/main/java/com/hmc/githubupload/MainActivity.java
+++ app/src/main/java/com/hmc/githubupload/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG,"xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        Log.d(TAG,"xxxxxxxxxxxxxxxxxxxxxxx");
     }
 
     @Override
